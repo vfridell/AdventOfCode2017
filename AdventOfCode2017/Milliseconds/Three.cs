5d4cdc310b117a64d2912441f0c2afbc89fc7f1b
vfridell <gusknows@gmail.com>
2017-12-04 17:12:09 -0500

    doop

---
+++ AdventOfCode2017/Milliseconds/Three.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017.Milliseconds
+{
+    public static class Three
+    {
+        public static void Part2()
+        {
+            string input = "";
+
+        }
+
+        public static void Part1()
+        {
+            string input = "368078";
+
+
+
+        }
+    }
+}
