fa43719a836d98665f3d771cecd146fbe823aab6
vfridell <gusknows@gmail.com>
2017-12-06 18:03:40 -0500

    bl

--- AdventOfCode2017/Milliseconds/Three.cs
+++ AdventOfCode2017/Milliseconds/Three.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdventOfCode2017.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +12,43 @@ namespace AdventOfCode2017.Milliseconds
     {
         public static void Part2()
         {
-            int input = 368078;
-            var rankList = new Dictionary<int, Rank>();
-            for (int i = 1; i < 100; i++) rankList.Add(i, new Rank(i));
+            Validate2();
 
+            int input = 368078;
+            int r = 3;
+            Rank prevRank = new Rank(2);
+            Rank currentRank = new Rank(r);
+            List<int> part2Values = new List<int> { 0, 1, 1,2,4,5,10,11,23,25 };
+            while(true)
+            {
+                int currentPart2Value;
+                for (int i = currentRank.smallestCell; i <= currentRank.seCorner; i++)
+                {
+                    currentPart2Value = 0;
+                    if (i > 2)
+                    {
+                        currentPart2Value = part2Values[i - 1];
+                        if (i > 3 && Adjacent(i, i - 2)) currentPart2Value += part2Values[i - 2];
+                    }
+
+                    for (int j = prevRank.smallestCell; j <= prevRank.seCorner; j++)
+                    {
+                        if (j == i - 1) continue;
+                        if (j == i - 2) continue;
+                        if (Adjacent(i, j)) currentPart2Value += part2Values[j];
+                    }
+
+                    part2Values.Add(currentPart2Value);
+                    Console.WriteLine($"Position {i} => {currentPart2Value}");
+                    if(currentPart2Value >= input)
+                    {
+                        Console.WriteLine($"Answer is {currentPart2Value}");
+                        return;
+                    }
+                }
+                prevRank = currentRank;
+                currentRank = new Rank(++r);
+            } 
         }
 
         public static void Part1()
@@ -53,6 +87,80 @@ namespace AdventOfCode2017.Milliseconds
             else return (int)(rankEst + 1);
         }
 
+        public static double Angle(int value)
+        {
+            Rank rank = new Rank(RankNum(value));
+            //double angle = 360.0 * ((rank.seCorner - (double)value) / (rank.seCorner - (double)rank.smallestCell - 1));
+            double angle = (rank.seCorner - (double)value) * rank.unitAngle;
+            return angle;
+        }
+
+        public static double AngleBetween(int value1, int value2)
+        {
+            double angle1 = Angle(value1);
+            double angle2 = Angle(value2);
+            double a = angle1 - angle2;
+
+            Func<double, double, double> mod = (v,n) => { return v - Math.Floor(v / n) * n; };
+            a = mod((a + 180), 360) - 180;
+            return Math.Abs(a);
+        }
+
+        public static double Distance(int value1, int value2)
+        {
+            Rank rank1 = new Rank(RankNum(value1));
+            Rank rank2 = new Rank(RankNum(value2));
+            double angleBetween = AngleBetween(value1, value2);
+            double radians = angleBetween * Math.PI / 180.0;
+            int distance1 = rank1.DistanceFromOrigin(value1);
+            int distance2 = rank2.DistanceFromOrigin(value2);
+
+            double length = (distance1 * distance1) + (distance2 * distance2) - (2 * distance1 * distance2 * Math.Cos(radians));
+            return length;
+        }
+
+        private static bool Adjacent(int currentCellNum, int otherCellNum)
+        {
+            if (currentCellNum - 1 == otherCellNum) return true;
+            if (otherCellNum == 1 && currentCellNum <= 9) return true;
+
+            if (currentCellNum <= otherCellNum) throw new Exception("current cell should be greater than the other cell");
+
+            Rank currentRank = new Rank(RankNum(currentCellNum));
+            Rank otherRank = new Rank(RankNum(otherCellNum));
+            if (currentRank.rank != otherRank.rank 
+                && currentRank.rank - 1 != otherRank.rank) throw new Exception("cell number must be in the current or prior rank to be adjacent");
+
+
+            double length = Distance(currentCellNum, otherCellNum);
+
+            return Math.Floor(length) <= 2;
+        }
+
+        private static void Validate2()
+        {
+            Console.WriteLine($"9 Adjacent to 2: {Adjacent(9, 2)}");
+            Console.WriteLine($"25 Adjacent to 24: {Adjacent(25, 24)}");
+            Console.WriteLine($"40 Adjacent to 11: {Adjacent(40, 11)}");
+
+            Rank rank1 = new Rank(1);
+            Rank rank2 = new Rank(2);
+            Rank rank3 = new Rank(3);
+            Rank rank4 = new Rank(4);
+            Rank rank5 = new Rank(5);
+
+            Rank rank = rank3;
+            for (int i = rank.smallestCell; i <= rank.seCorner; i++)
+            {
+                for (int j = rank.smallestCell; j <= rank.seCorner; j++)
+                {
+                    //Console.WriteLine($"Angle between {i} and {j} is {AngleBetween(i, j)}");
+                    Console.WriteLine($"Distance between {i} and {j} is {Distance(i, j)}");
+                }
+            }
+
+        }
+
         private static void Validate()
         {
             List<int> corners = new List<int>();
@@ -76,6 +184,9 @@ namespace AdventOfCode2017.Milliseconds
         }
     }
 
+
+
+
     public class Rank
     {
         public int rank;
@@ -90,6 +201,7 @@ namespace AdventOfCode2017.Milliseconds
         public int north;
         public int east;
         public int smallestCell;
+        public double unitAngle;
 
         public Dictionary<int, int> part2List;
 
@@ -106,7 +218,8 @@ namespace AdventOfCode2017.Milliseconds
             west = swCorner - (LengthOfSide / 2);
             north = nwCorner - (LengthOfSide / 2);
             east = neCorner - (LengthOfSide / 2);
-            smallestCell = east - (LengthOfSide / 2) + 1;
+            smallestCell = n==1 ? 1 : east - (LengthOfSide / 2) + 1;
+            unitAngle = 360.0 * (  1.0 / (seCorner - (smallestCell - 1)));
         }
 
         public bool CellNumInRank(int cellValue)
@@ -134,28 +247,28 @@ namespace AdventOfCode2017.Milliseconds
             return distances.Min();
         }
 
-        public List<int> GetPart2Values()
+        public int SignedDistanceFromNearestSide(int cellValue)
         {
-            if (part2List == null) throw new Exception("Must init values first");
-            return part2List.Values.ToList();
+            if (!CellNumInRank(cellValue))
+                throw new Exception($"Value {cellValue} is not on rank {rank}");
+
+            int[] distances = {
+                east - cellValue,
+                north - cellValue,
+                west - cellValue,
+                south - cellValue,
+            };
+
+            return distances.Min(d => Math.Abs(d));
         }
 
-        public void CalcPart2Values(Rank lastRank)
+        public Coordinate GetCoordinate(int cellValue)
         {
-            if (rank == 1) part2List = new Dictionary<int, int>() { { 1, 1 } };
 
-            for(int i = smallestCell; i<= seCorner; i++)
-            {
-
-            }
         }
 
-        //private int Distance(int currentCellNum, int otherCellNum, Rank lastRank)
-        //{
-        //    if (!lastRank.CellNumInRank(otherCellNum) || CellNumInRank(otherCellNum)) throw new Exception("cell number must be in the current or prior rank to be adjacent");
 
-        //    if (currentCellNum - 1 == otherCellNum) return 1;
 
-        //}
     }
+
 }
---
+++ AdventOfCode2017/Models/Coordinate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017.Models
+{
+    public enum Direction
+    {
+        Left = 60,  // <
+        Right = 62, // >
+        Up = 43,    // +
+        Down = 45   // -
+    }
+
+        public struct Coordinate
+        {
+            public Coordinate(int row, int column)
+            {
+                Row = row;
+                Column = column;
+            }
+            public int Row;
+            public int Column;
+
+            public override string ToString() => $"({Row},{Column})";
+
+            public static Coordinate operator +(Coordinate c1, Coordinate c2) => new Coordinate(c1.Row + c2.Row, c1.Column + c2.Column);
+            public static int Distance(Coordinate c1, Coordinate c2) => Math.Abs(c1.Row - c2.Row) + Math.Abs(c1.Column - c2.Column);
+            public static bool operator ==(Coordinate c1, Coordinate c2) => c1.Row == c2.Row && c1.Column == c2.Column;
+            public static bool operator !=(Coordinate c1, Coordinate c2) => c1.Row != c2.Row || c1.Column != c2.Column;
+
+        }
+}
